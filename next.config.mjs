5c9d216d623b710ab92bd1f9f4d23ed5c4a3fd2a
Abdul Hanan <85007252+abdulhanan0@users.noreply.github.com>
2024-07-30 19:07:12 +0500

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
