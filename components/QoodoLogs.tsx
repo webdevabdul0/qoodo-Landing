e522313e948ccd218a222e4be5604e141e5e98c5
Abdul Hanan <85007252+abdulhanan0@users.noreply.github.com>
2024-08-02 20:50:08 +0500

    Img

--- components/QoodoLogs.tsx
+++ components/QoodoLogs.tsx
@@ -53,24 +53,12 @@ const QoodoLogs = () => {
           </p>
         </div>
 
-        <Image
-          src={"/images/Log/3.png"}
-          width={900}
-          height={500}
-          alt="Log"
-          style={{ width: "100%", height: "auto" }}
-        />
+        <Image src={"/images/Log/3.png"} width={700} height={1} alt="Log" />
       </div>
 
       {/*Log Feature Style 2*/}
       <div className=" px-0 md:px-[100px] py-[50px]  bg-opacity-10 bg-gradient-to-b from-neutral-950 via-[#4a60ff]/30 to-neutral-950  flex flex-col-reverse lg:flex-row md:justify-between items-center  gap-[20px] md:gap-[100px] ">
-        <Image
-          src={"/images/Log/4.png"}
-          width={900}
-          height={500}
-          alt="Log"
-          style={{ width: "100%", height: "auto" }}
-        />
+        <Image src={"/images/Log/4.png"} width={700} height={1} alt="Log" />
 
         <div className="w-full flex flex-col justify-center items-center lg:items-start gap-5 text-center lg:text-start">
           {/*Small Gradient badge*/}
@@ -102,24 +90,12 @@ const QoodoLogs = () => {
           </p>
         </div>
 
-        <Image
-          src={"/images/Log/5.png"}
-          width={900}
-          height={500}
-          alt="Log"
-          style={{ width: "100%", height: "auto" }}
-        />
+        <Image src={"/images/Log/5.png"} width={700} height={1} alt="Log" />
       </div>
 
       {/*Log Feature Style 2*/}
       <div className=" px-0 md:px-[100px] py-[50px]  bg-opacity-10 bg-gradient-to-b from-neutral-950 via-[#4a60ff]/30 to-neutral-950  flex flex-col-reverse lg:flex-row md:justify-between items-center  gap-[20px] md:gap-[100px] ">
-        <Image
-          src={"/images/Log/6.png"}
-          width={900}
-          height={500}
-          alt="Log"
-          style={{ width: "100%", height: "auto" }}
-        />
+        <Image src={"/images/Log/6.png"} width={700} height={1} alt="Log" />
 
         <div className="w-full flex flex-col justify-center items-center lg:items-start gap-5 text-center lg:text-start">
           {/*Small Gradient badge*/}
