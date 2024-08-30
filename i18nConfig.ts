74915cb104e09cab76d80042e39beb877726aa88
Abdul Hanan <85007252+abdulhanan0@users.noreply.github.com>
2024-08-30 18:52:01 +0500

    Update i18nConfig.ts

--- i18nConfig.ts
+++ i18nConfig.ts
@@ -2,7 +2,7 @@ import { Config } from "next-i18n-router/dist/types";
 
 const i18nConfig: Config = {
   locales: ["en", "hu"],
-  defaultLocale: "en",
+  defaultLocale: "hu",
 };
 
 export default i18nConfig;
