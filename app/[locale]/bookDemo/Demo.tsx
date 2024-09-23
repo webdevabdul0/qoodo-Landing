97b7d68ee4af2c1ea1c10e2819b8ed9e28c226c6
noorulhassan364 <noorulhassan364@gmail.com>
2024-09-23 06:41:43 +0500

    contact us Api integrated

--- app/[locale]/bookDemo/Demo.tsx
+++ app/[locale]/bookDemo/Demo.tsx
@@ -49,7 +49,7 @@ const Demo = () => {
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
     try {
-      const response = await fetch("/api/sendmail", {
+      const response = await fetch("https://dev-apis.naplozz.hu/api/v1/users/sendDemoMail", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
