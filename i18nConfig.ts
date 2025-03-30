f90e94957dbc0ffa909de50b413ef16ab7257f5c
webdevabdul0 <ah0918047@gmail.com>
2025-03-30 19:44:21 +0500

    Change

--- i18nConfig.ts
+++ i18nConfig.ts
@@ -1,8 +1,9 @@
 import { Config } from "next-i18n-router/dist/types";
 
 const i18nConfig: Config = {
-  locales: ["hu", "en", "es"], // Order matters! Ensure "es" is first
-  defaultLocale: "hu", // Default should be "es"
+  locales: ["es", "en", "hu"],
+  defaultLocale: "hu",
+  localeDetector: false,
 };
 
 export default i18nConfig;
