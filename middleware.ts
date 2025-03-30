9c6c5c7d9dd1e525eb04fe62454e23f69ea94cf3
webdevabdul0 <ah0918047@gmail.com>
2025-03-30 15:33:39 +0500

    Changes

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,29 @@
 import { i18nRouter } from "next-i18n-router";
 import i18nConfig from "./i18nConfig";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
+  const hostname = request.headers.get("host") || "";
+
+  // Define domain-to-locale mapping
+  const domainLocales: Record<string, string> = {
+    "qoodo.hu": "hu",
+    "qoodo.es": "es",
+  };
+
+  // Determine the locale based on the domain
+  const locale = domainLocales[hostname];
+
+  if (locale && !url.pathname.startsWith(`/${locale}`)) {
+    url.pathname = `/${locale}${url.pathname}`;
+    return NextResponse.redirect(url);
+  }
+
   return i18nRouter(request, i18nConfig);
 }
 
-// only applies this middleware to files in the app directory
+// Only applies middleware to specific routes
 export const config = {
   matcher: "/((?!api|static|.*\\..*|_next).*)",
 };
